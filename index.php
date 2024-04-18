1436195b33d1d772c990a0caf2efc385a90eb741
SteijlenGLR <steijlen@glr.nl>
2024-04-18 09:02:27 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$data = array(
+    "naam" => "Jan Janssen",
+    " leeftijd" => 35,
+    "hobbies" => ["voetbal", "lezen", "films"],
+    "adres" => array(
+        "straat" => "Hoofdstraat",
+        "huisnummer" => 17,
+        "plaats" => "Amsterdam"
+    )
+);
+
+
+
+include_once ('views/index_view.php');
---
+++ views/index_view.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PROGRAM2 - OOP</title>
+</head>
+<body>
+<h1>Eerste demo, gebruik van functies</h1>
+<p>Deze pagina toont de inhoud van een array</p>
+
+</body>
+</html>
